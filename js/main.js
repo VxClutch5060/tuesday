0194a7ec2ca137ddaf25e6e441a5db4adced022a
VxClutch5060 <98831688+VxClutch5060@users.noreply.github.com>
2023-02-24 17:36:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
-function isValid(){
+function isValid() {
   var password = document.getElementById('password').value;
   var username = document.getElementById('username').value;
-  if (password == "admin1" && username == "admin1")
-  {window.location = 'employeepage.html'}
+  if (password == "admin1" && username == "Marko") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "Owen7227" && username == "Owen") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "sussymog" && username == "Sam") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "admin1" && username == "Marko") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "admin1" && username == "Naina") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "admin1" && username == "Mat") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "admin1" && username == "Zak") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "admin1" && username == "Greyson") {
+    window.location = 'employeepage.html'
+  }
+  else if (password == "scrubdaddy69" && username == "Landen") {
+    window.location = 'employeepage.html'
+  }
   else
-  {alert('Wrong Password or Username')}
+  {alert('Non Existant username or password. Try again.')}
   }
